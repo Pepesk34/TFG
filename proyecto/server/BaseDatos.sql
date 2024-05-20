4170eca4805f8dddaa841efa237145d4672f3a75
Pepe <pepevic.dalias@gmail.com>
2024-05-20 18:09:47 +0200

    cambio a serial

--- proyecto/server/BaseDatos.sql
+++ proyecto/server/BaseDatos.sql
@@ -2,20 +2,20 @@ CREATE DATABASE `greenboydhousedb`;
 USE greenboydhousedb;
 
 CREATE TABLE agricultores (
-    id INT(10) AUTO_INCREMENT PRIMARY KEY,
+    id INT(10) serial PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     apellidos VARCHAR(100) NOT NULL,
-    dni VARCHAR(9) NOT NULL,
+    dni VARCHAR(9) unique NOT NULL,
     usuario VARCHAR(100) NOT NULL,
     contraseña VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE voluntarios (
-    id INT(10) AUTO_INCREMENT PRIMARY KEY,
+    id INT(10) serial PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     apellidos VARCHAR(100) NOT NULL,
-    dni VARCHAR(9) NOT NULL,
-    matricula VARCHAR(20) NOT NULL,
+    dni VARCHAR(9) unique NOT NULL,
+    matricula VARCHAR(20) unique NOT NULL,
     usuario VARCHAR(100) NOT NULL,
     contraseña VARCHAR(100) NOT NULL
 );
