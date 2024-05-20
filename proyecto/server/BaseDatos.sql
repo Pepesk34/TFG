dd3f61e7fc3414d7fb3a5ca72e44cf54197746e8
Adrixdlol <adrianperezalonso5000@gmail.com>
2024-05-20 10:42:30 +0200

    db actualizada

--- proyecto/server/BaseDatos.sql
+++ proyecto/server/BaseDatos.sql
@@ -9,3 +9,13 @@ CREATE TABLE agricultores (
     usuario VARCHAR(100) NOT NULL,
     contraseña VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE voluntarios (
+    id INT(10) AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    apellidos VARCHAR(100) NOT NULL,
+    dni VARCHAR(9) NOT NULL,
+    matricula VARCHAR(20) NOT NULL,
+    usuario VARCHAR(100) NOT NULL,
+    contraseña VARCHAR(100) NOT NULL
+);
