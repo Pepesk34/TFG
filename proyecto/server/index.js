261d758ab4e3c78d3b5a196b9fa32001eb74f513
Pepe <pepevic.dalias@gmail.com>
2024-05-20 19:38:43 +0200

    cambio a postgresql

--- proyecto/server/index.js
+++ proyecto/server/index.js
@@ -1,21 +1,29 @@
-const express = require("express"); //importar express
+import express from 'express' //importar express
+import pg from 'pg';
+import cors from 'cors';
+
 const app = express()
-const mysql = require("mysql"); //importamos mysql
-const cors = require("cors");
+
 
 app.use(cors());
-app.use(express.json());
-//Creamos conexión a la bd
-const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"greenboydhousedb"
+app.use(express.json()); 
+
+// Configura la conexión
+const db = new pg.Client({
+    user: 'postgres',         // reemplaza con tu usuario de PostgreSQL
+    host: 'localhost',          // dirección del servidor PostgreSQL
+    database: 'greenboydhousedb', // reemplaza con el nombre de tu base de datos
+    password: '8569',            // reemplaza con tu contraseña
+    port: 5432,                 // puerto donde corre PostgreSQL
 });
 
 //definimos la ruta para manejar las solicitudes post
-app.post("/create",(req,res)=>{
 
+db.connect();
+
+app.post("/create", (req, res)=> {
+
+    console.log("entra en create");
     //Obtenemos datos del cuerpo de la solicitud
     const nombre = req.body.nombre;
     const apellidos = req.body.apellidos;
@@ -25,18 +33,19 @@ app.post("/create",(req,res)=>{
 
     //consulta sql
     db.query(
-        "INSERT INTO agricultores(nombre, apellidos, dni, usuario, contraseña) VALUES (?, ?, ?, ?, ?)",
+        "INSERT INTO agricultores(nombre, apellidos, dni, usuario, contraseña) VALUES ($1, $2, $3, $4, $5)",
         [nombre, apellidos, dni, usuario, contraseña],
         (err, result) => {
             if (err) {
-                console.log(err);
+                console.log('Error al hacer el insert' + err);
                 res.status(500).send("Error al registrar el agricultor");
             } else {
-                res.send("Registrado con éxito el agricultor");
+                res.send("Registro completado con exito")
             }
         }
     );
 });
+
 
  //Iniciamos el servidor en el puerto 3001
  app.listen(3001, ()=>{
