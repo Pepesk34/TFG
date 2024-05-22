3087ea34a1d421c175ba28e5e63eac79d39edd2a
Adrixdlol <adrianperezalonso5000@gmail.com>
2024-05-22 16:53:56 +0200

    css recuperado

--- proyecto/client/src/components/componentes_cuerpo/registrarse.js
+++ proyecto/client/src/components/componentes_cuerpo/registrarse.js
@@ -1,14 +1,14 @@
 import '../estilo.css';
 import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
-import agricultor from "../imagenes/agricultor.png";
-import voluntario from "../imagenes/voluntario.png"
+import agricultor from "../imagenes/agricultor.jpg";
+import voluntario from "../imagenes/voluntario.jpg"
 const Registrarse = () => {
     return (
-        <div>
+        <div id="div_registrarse">
             <div className='row'>
                 <div className='col-xl-12'>
-                    <div id="preguntarol">
-                        <h3>¿Cual va a ser tu rol?</h3>
+                    <div id="pregunta_rol">
+                        <h1>¿Cuál va a ser tu rol?</h1>
                     </div>
 
                 </div>
@@ -16,15 +16,17 @@ const Registrarse = () => {
             <div className='row'>
                 <div className='col-xl-6'>
                     <Link to="/agricultor">
-                        <img src={agricultor} />
+                        <img id="img_agricultor" src={agricultor} />
                     </Link>
+                    <h3 id="h3_agricultor">Agricultor</h3>
+                    
 
                 </div>
                 <div className='col-xl-6'>
                     <Link to="voluntario">
-                    <img src={voluntario} />
+                    <img id="img_voluntario"src={voluntario} />
                     </Link>
-                    
+                    <h3 id="h3_voluntario">Voluntario</h3>
                 </div>
             </div>
         </div>
