5e4493af8ec9ef852ef970cad4be146b1e64fa97
pepesk34 <pepevic.dalias@gmail.com>
2024-05-31 14:59:27 +0200

    modal success by pepe

--- proyecto/client/src/components/cabecerarecolecta.js
+++ proyecto/client/src/components/cabecerarecolecta.js
@@ -1,23 +1,11 @@
 // src/components/CabeceraRecolecta.js
-import React from 'react';
 import './estilo.css';
 import logo from "./imagenes/logo1.jpg";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BotonCabecera from './botonCabecera';
 import { Link } from "react-router-dom";
-import { useState } from "react";
-
-const handleScrollToTop = () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-};
-
-const handleClick = () => {
-    console.log('Botón clickeado');
-    handleScrollToTop();
-};
+import React, { useState } from "react";
+import { Modal, Button } from 'react-bootstrap';
 
 function CabeceraRecolecta() {
     const [showModal, setShowModal] = useState(false);
@@ -25,6 +13,12 @@ function CabeceraRecolecta() {
     const toggleModal = () => {
         setShowModal(!showModal);
     };
+
+    
+      const handleCloseModal = () => setShowModal(false);
+      const handleShowModal = () => {
+        setShowModal(true);
+      };
 
     return (
         <div id="cabecera-recolecta">
@@ -43,7 +37,7 @@ function CabeceraRecolecta() {
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" id='nav-filtros' to="/registrarse" onClick={handleClick}>
+                            <Link className="nav-link" id='nav-filtros' to="/registrarse">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-sort-alpha-down" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M10.082 5.629 9.664 7H8.598l1.789-5.332h1.234L13.402 7h-1.12l-.419-1.371zm1.57-.785L11 2.687h-.047l-.652 2.157z" />
                                     <path d="M12.96 14H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645zM4.5 2.5a.5.5 0 0 0-1 0v9.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L4.5 12.293z" />
@@ -51,7 +45,8 @@ function CabeceraRecolecta() {
                             </Link>
                         </li>
                         <li className="nav-item btn-group" id='nav-perfil'>
-                            <button className="nav-link" type='button' id='button-perfil'>
+                            <button className="btn btn-dark" type='button' id='button-perfil'
+                  onClick={() => handleShowModal()}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                                     <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
@@ -59,7 +54,7 @@ function CabeceraRecolecta() {
                             </button>
                         </li>
                         <li className="nav-item btn-group" id='nav-ajustes'>
-                            <button className="btn btn-dark" type='button' id='button-ajustes'>
+                            <button className="btn btn-dark" type='button' id='button-ajustes' >
                                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-question-circle" viewBox="0 0 16 16">
                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
                                     <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286m1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94" />
@@ -69,6 +64,22 @@ function CabeceraRecolecta() {
                     </ul>
                 </div>
             </nav>
+            <Modal show={showModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>hola</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    hola1
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseModal}>
+                        Cerrar
+                    </Button>
+                    <Button variant="primary" onClick={handleCloseModal}>
+                        Guardar cambios
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 }
