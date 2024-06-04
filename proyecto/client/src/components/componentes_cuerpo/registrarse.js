03dfe175eac3d7da42449866531d54378ff27de2
Pablo <pablo.cano@viewnext.com>
2024-06-04 13:49:31 +0200

    intento de cambio registrarse

--- proyecto/client/src/components/componentes_cuerpo/registrarse.js
+++ proyecto/client/src/components/componentes_cuerpo/registrarse.js
@@ -1,45 +1,51 @@
-    import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
-    import agricultor from "../imagenes/imagen3.jpg";
-    import voluntario from "../imagenes/voluntario.jpg";
-    import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
+import agricultor from "../imagenes/imagen3.jpg";
+import voluntario from "../imagenes/voluntario.jpg";
+import { useEffect } from "react";
 
 
-    const Registrarse = () => {
+const Registrarse = () => {
 
-        useEffect(() => {
-            window.scrollTo(0, 260);
-        }, []);
-        
+    useEffect(() => {
+        window.scrollTo(0, 260);
+    }, []);
 
-        return (
-            <div id="registrarse_div">
-                <div className='row'>
-                    <div className='col-xl-12'>
-                        <div id="pregunta_rol">
-                            <h1>¿Cuál va a ser tu rol?</h1>
-                        </div>
 
+    return (
+        <div id="registrarse-div">
+            <div className='row'>
+                <div className='col-xl-12'>
+                    <div id="pregunta_rol">
+                        <h1>¿Cuál va a ser tu rol?</h1>
                     </div>
-                </div>
-                <div id="opciones_registrarse" className='row'>
-                    <div id="div_agricultor_rol" className='col-xl-6'>
-                        <h3 id="h3_agricultor">Agricultor</h3>
-                        <Link to="/agricultor">
-                            <img id="emoji_agricultor" src={agricultor} width={"50px"}/>
-                        </Link>
 
-                    </div>
-                    <div id="div_voluntario_rol"className='col-xl-6'>
-                        <h3 id="h3_voluntario">Voluntario</h3>
-                        <Link to="/voluntario">
-                            <img id="emoji_voluntario"src={voluntario} />  
-                        </Link>
-
-                    </div>
                 </div>
             </div>
+            <div id="opciones-registrarse" className='row'>
+                <div className='col-xl-6'>
+                    <h3 id="h3-agricultor">Agricultor</h3>
+                    <div className="img-container">
+                        <Link to="/agricultor">
+                            <img id="emoji-agricultor" src={agricultor} />
+                            <div className="img-descripcion">El agricultor cultiva y proporciona alimentos frescos.</div>
+                        </Link>
+                    </div>
 
-        );
+                </div>
+                <div className='col-xl-6'>
+                    <h3 id="h3-voluntario">Voluntario</h3>
+                    <div className="img-container">
+                        <Link to="/voluntario">
+                            <img id="emoji-voluntario" src={voluntario} />
+                            <div className="img-descripcion">El voluntario ayuda a recolectar y distribuir alimentos.</div>
+                        </Link>
+                    </div>
 
-    }
-    export default Registrarse
+                </div>
+            </div>
+        </div>
+
+    );
+
+}
+export default Registrarse
