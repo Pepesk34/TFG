fc7af0ef3f34928e4b3afb4d5b1a4d27bf0c9667
Pablo <pablo.cano@viewnext.com>
2024-06-05 08:58:02 +0200

    row frutas

--- proyecto/client/src/components/componentes_cuerpo/about.js
+++ proyecto/client/src/components/componentes_cuerpo/about.js
@@ -12,22 +12,22 @@ function About() {
 
     return(
         <div className='contenedor' id='about-main'>
-            <div className='row about-fruta'>
+            <div className='row about-tomate'>
                 <div className='col-6'>
                     <h1>HOLA</h1>
                 </div>
             </div>
-            <div className='row about-fruta'>
+            <div className='row about-pimiento'>
                 <div className='col-6'>
                     <h1>HOLA</h1>
                 </div>
             </div>
-            <div className='row about-fruta'>
+            <div className='row about-berenjena'>
                 <div className='col-6'>
                     <h1>HOLA</h1>
                 </div>
             </div>
-            <div className='row about-fruta'>
+            <div className='row about-calabacin'>
                 <div className='col-6'>
                     <h1>HOLA</h1>
                 </div>
